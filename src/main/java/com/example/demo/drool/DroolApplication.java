ebf6b2ea65149634f2426b74f3ca2fdaf9ec1d74
Suro <surobinda@gmail.com>
2025-05-24 18:59:27 +0530

    Just few minor correction.

--- src/main/java/com/example/demo/drool/DroolApplication.java
+++ src/main/java/com/example/demo/drool/DroolApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DroolApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DroolApplication.class, args);
 	}
-
 }
